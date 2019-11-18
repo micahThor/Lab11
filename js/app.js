9a5a6f8f79e68ba810b825c72ef486f73f9de283
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2019-11-18 15:23:57 -0800

    refactored product object

--- js/app.js
+++ js/app.js
@@ -2,48 +2,81 @@
 
 function main() {
   // create product manager and add products
-  var productMgr = new ProductManager();
+  Product.addProduct(new Product('bag', 'img/bag.jpg'));
+  Product.addProduct(new Product('banana', 'img/banana.jpg'));
+  Product.addProduct(new Product('bathroom', 'img/bathroom.jpg'));
+  Product.addProduct(new Product('boots', 'img/boots.jpg'));
+  Product.addProduct(new Product('breakfast', 'img/breakfast.jpg'));
+  Product.addProduct(new Product('bubblegum', 'img/bubblegum.jpg'));
+  Product.addProduct(new Product('chair', 'img/chair.jpg'));
+  Product.addProduct(new Product('cthulhu', 'img/cthulhu.jpg'));
+  Product.addProduct(new Product('dog-duck', 'img/dog-duck.jpg'));
+  Product.addProduct(new Product('dragon', 'img/dragon.jpg'));
+  Product.addProduct(new Product('pen', 'img/pen.jpg'));
+  Product.addProduct(new Product('pet-sweep', 'img/pet-sweep.jpg'));
+  Product.addProduct(new Product('scissors', 'img/scissors.jpg'));
+  Product.addProduct(new Product('shark', 'img/shark.jpg'));
+  Product.addProduct(new Product('sweep', 'img/sweep.png'));
+  Product.addProduct(new Product('tauntaun', 'img/tauntaun.jpg'));
+  Product.addProduct(new Product('unicorn', 'img/unicorn.jpg'));
+  Product.addProduct(new Product('usb', 'img/usb.gif'));
+  Product.addProduct(new Product('water-can', 'img/water-can.jpg'));
+  Product.addProduct(new Product('wine-glass', 'img/wine-glass.jpg'));
+  // set initial images on page load
+  Product.setRandomImages();
 
-  productMgr.addProduct(new Product('bag', 'img/bag.jpg'));
-  productMgr.addProduct(new Product('banana', 'img/banana.jpg'));
-  productMgr.addProduct(new Product('bathroom', 'img/bathroom.jpg'));
-  productMgr.addProduct(new Product('boots', 'img/boots.jpg'));
-  productMgr.addProduct(new Product('breakfast', 'img/breakfast.jpg'));
-  productMgr.addProduct(new Product('bubblegum', 'img/bubblegum.jpg'));
-  productMgr.addProduct(new Product('chair', 'img/chair.jpg'));
-  productMgr.addProduct(new Product('cthulhu', 'img/cthulhu.jpg'));
-  productMgr.addProduct(new Product('dog-duck', 'img/dog-duck.jpg'));
-  productMgr.addProduct(new Product('dragon', 'img/dragon.jpg'));
-  productMgr.addProduct(new Product('pen', 'img/pen.jpg'));
-  productMgr.addProduct(new Product('pet-sweep', 'img/pet-sweep.jpg'));
-  productMgr.addProduct(new Product('scissors', 'img/scissors.jpg'));
-  productMgr.addProduct(new Product('shark', 'img/shark.jpg'));
-  productMgr.addProduct(new Product('sweep', 'img/sweep.png'));
-  productMgr.addProduct(new Product('tauntaun', 'img/tantaun.jpg'));
-  productMgr.addProduct(new Product('unicorn', 'img/unicorn.jpg'));
-  productMgr.addProduct(new Product('usb', 'img/usb.gif'));
-  productMgr.addProduct(new Product('water-can', 'img/water-can.jpg'));
-  productMgr.addProduct(new Product('wine-glass', 'img/wine-glass.jpg'));
-
-  
 }
 
 // Product represents an item in store
 function Product(name, imgUrl) {
+  // instance variables
   this.name = name;
   this.imgUrl = imgUrl;
   this.clickCount = 0;
   this.viewCount = 0;
 }
 
-function ProductManager() {
-  this.productList = [];
-  this.totalVoteCount = 0;
+// 
+Product.clickCount = 0;
+
+// The amount of total votes
+Product.totalVoteCount = 0;
+// The maximum allowed votes
+Product.maxVoteCount = 25;
+
+Product.productList = [];
+
+Product.addProduct = function(product) {
+  Product.productList.push(product);
 }
 
-ProductManager.prototype.addProduct(product) {
-  this.productList.push(product);
+// set html img element variables
+Product.leftImage = document.getElementById('leftProduct');
+Product.centerImage = document.getElementById('centerProduct');
+Product.rightImage = document.getElementById('rightProduct');
+// add event listeners to img elements
+Product.leftImage.addEventListener('click', clickHandler);
+Product.centerImage.addEventListener('click', clickHandler);
+Product.rightImage.addEventListener('click', clickHandler);
+
+
+// Function getRandomProductImage returns a random product from the ProductManager's productlist
+Product.getRandomProductImage = function () {
+  return Product.productList[Math.floor(Math.random() * Product.productList.length)].imgUrl;
 }
+
+Product.setRandomImages = function () {
+  Product.leftImage.src = Product.getRandomProductImage();
+  Product.centerImage.src = Product.getRandomProductImage();
+  Product.rightImage.src = Product.getRandomProductImage();
+}
+
+
+function clickHandler(event) {
+  console.log(event.target.alt);
+  Product.setRandomImages();
+}
+
 
 
 
